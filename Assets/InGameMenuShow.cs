d1740bf01580e68086d974ddb6c7937a6238ad89
WhiteAlucard <claudio.oulego@gmail.com>
2022-05-14 13:59:40 -0300

    refinement

---
+++ Assets/InGameMenuShow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenuShow : MonoBehaviour
+{
+    public GameObject menuJuego;
+    public bool show;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool Show;
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            show = !show;
+            menuJuego.SetActive(show);
+        }
+    }
+}
--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class InGameMenu : MonoBehaviour
 {
+    public GameObject menuJuego;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +19,10 @@ public class InGameMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            menuJuego.SetActive(true);
+        }
     }
 
     public void PlayGame()
